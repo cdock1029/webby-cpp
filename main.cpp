f3b7b55bd3961ecd812553e3a5e4878fc4e33a4c
Conor Dockry <conordockry@gmail.com>
2023-10-18 00:33:00 -0400

    tmp change

--- main.cpp
+++ main.cpp
@@ -1,12 +1,18 @@
-#include <iostream>
 
-int me()
-{
-    return 1;
-}
+#include "crow/app.h"
+constexpr int PORT = 4000;
+
+using namespace crow;
 
 int main()
 {
-    me();
-    std::cout << "Hello, from webby!\n";
+    SimpleApp app;
+
+    CROW_ROUTE(app, "/").methods("GET"_method)([]() {
+        const auto page = mustache::load("index.html");
+        mustache::context ctx({ { "name", "Freya" } });
+        return page.render(ctx);
+    });
+
+    app.port(PORT).multithreaded().run();
 }
